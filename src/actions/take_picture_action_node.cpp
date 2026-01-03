9a569ddc05358b2b40dd11e48558dd538c4aec83
paololais <paolo.lais1@gmail.com>
2026-01-03 14:41:07 +0100

    Add skeleton action nodes for taking pictures and detecting markers

---
+++ src/actions/take_picture_action_node.cpp
@@ -0,0 +1,50 @@
+#include "plansys2_executor/ActionExecutorClient.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+using namespace std::chrono_literals;
+
+class AskCharge : public plansys2::ActionExecutorClient
+{
+public:
+  AskCharge()
+  : plansys2::ActionExecutorClient("take_picture", 1s)
+  {
+    progress_ = 0.0;
+  }
+
+private:
+  void do_work()
+  {
+    if (progress_ < 1.0) {
+      progress_ += 0.1;
+      send_feedback(progress_, "TakePicture running");
+    } else {
+      finish(true, 1.0, "TakePicture completed");
+
+      progress_ = 0.0;
+      std::cout << std::endl;
+    }
+
+    std::cout << "\r\e[K" << std::flush;
+    std::cout << "Requesting for taking picture ... [" << std::min(100.0, progress_ * 100.0) << "%]  " <<
+      std::flush;
+  }
+
+  float progress_;
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<AskCharge>();
+
+  node->set_parameter(rclcpp::Parameter("action_name", "take_picture"));
+  node->trigger_transition(lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE);
+
+  rclcpp::spin(node->get_node_base_interface());
+
+  rclcpp::shutdown();
+
+  return 0;
+}
